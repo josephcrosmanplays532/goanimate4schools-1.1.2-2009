abb348e27dd62347d0616424a043542e1895a147
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-02-21 17:36:01 -0500

    videomaker glitch fixes

--- static/page.js
+++ static/page.js
@@ -35,7 +35,7 @@ module.exports = function (req, res, url) {
 				flashvars: {
 					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
 					'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go', 'lid': 13, 'isLogin': 'Y', 'retut': 1,
-					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
+					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'retro', 'tlang': 'en_US',
 					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/pages/html/list.html',
 				},
 				allowScriptAccess: 'always',
